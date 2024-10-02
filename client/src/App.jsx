364dcb12f18d6482363b805029342ac479c2692c
rutika-creater <68539760+rutika-creater@users.noreply.github.com>
2024-10-02 23:56:45 +0530

    client added

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Tododashboard from './components/Tododashboard'
+
+
+function App() {
+  return (
+    <>
+    <Tododashboard/>
+    </>
+  )
+}
+
+export default App
